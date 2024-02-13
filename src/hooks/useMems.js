6c3f88aa21ac8edf0e40f5ad637347d25bb89517
pajacowo <github@pajacowo.pl>
2024-02-13 21:46:54 +0100

    Connect to backend API

--- src/hooks/useMems.js
+++ src/hooks/useMems.js
@@ -1,57 +1,59 @@
 import { useQuery } from "@tanstack/react-query";
 import { useCallback } from "react";
-//import { MEMS_URL } from "./consts";
+import { MEMS_URL } from "./consts";
 import { queryKeys } from "../react-query/consts";
 import { filterMems } from "./utils";
 import { useToast } from "./useToast";
 
 async function getMems() {
-  //const data = await fetch(MEMS_URL);
+  const data = await fetch(MEMS_URL);
 
-  const db = [
-    {
-      id: 1,
-      title: "Kiedy zbliża się Wielkanoc",
-      upvotes: 14,
-      downvotes: 7,
-      img: "../img/mems/mem-1.jpg",
-    },
-    {
-      id: 2,
-      title: "Oczekiwania a rzeczywistość",
-      upvotes: 9,
-      downvotes: 4,
-      img: "../img/mems/mem-2.jpg",
-    },
-    {
-      id: 3,
-      title: "Architektura IT",
-      upvotes: 16,
-      downvotes: 5,
-      img: "../img/mems/mem-3.png",
-    },
-    {
-      id: 4,
-      title: "Gdy nieubłaganie zbliża się deadline",
-      upvotes: 7,
-      downvotes: 6,
-      img: "../img/mems/mem-4.jpg",
-    },
-    {
-      title: "Junior vs Senior",
-      upvotes: 3,
-      downvotes: 1,
-      img: "../img/mems/maxresdefault.jpg",
-      id: 5,
-    },
-  ];
+  // console.log("Data: ", data.json());
 
-  const data = new Promise(function (myResolve, myReject) {
-    myResolve(db);
-  });
+  // const db = [
+  //   {
+  //     id: 1,
+  //     title: "Kiedy zbliża się Wielkanoc",
+  //     upvotes: 14,
+  //     downvotes: 7,
+  //     img: "../img/mems/mem-1.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Oczekiwania a rzeczywistość",
+  //     upvotes: 9,
+  //     downvotes: 4,
+  //     img: "../img/mems/mem-2.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Architektura IT",
+  //     upvotes: 16,
+  //     downvotes: 5,
+  //     img: "../img/mems/mem-3.png",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Gdy nieubłaganie zbliża się deadline",
+  //     upvotes: 7,
+  //     downvotes: 6,
+  //     img: "../img/mems/mem-4.jpg",
+  //   },
+  //   {
+  //     title: "Junior vs Senior",
+  //     upvotes: 3,
+  //     downvotes: 1,
+  //     img: "../img/mems/maxresdefault.jpg",
+  //     id: 5,
+  //   },
+  // ];
 
-  //return data.json();
-  return data;
+  // data = new Promise(function (myResolve, myReject) {
+  //   myResolve(db);
+  // });
+
+  return data.json();
+  // return data;
 }
 
 export const useMems = (type) => {
