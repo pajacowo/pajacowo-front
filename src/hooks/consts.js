91acdb2c8f73af2416db7d2b11517ef0c61a3c0b
pajacowo <github@pajacowo.pl>
2024-02-17 18:41:57 +0100

    Add .env

--- src/hooks/consts.js
+++ src/hooks/consts.js
@@ -1,4 +1,3 @@
-//export const MEMS_URL = "http://localhost:3000/mems";
-export const MEMS_URL = "http://127.0.0.1:8000/api/mems";
+export const MEMS_URL = "http://127.0.0.1:8000/api/mems"
 export const UPLOAD_URL = "http://localhost:3001/upload";
 
